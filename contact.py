5b87f239aa384e48e2eac74905b4a2b690bcd32e
melagin <melagin@fix.ru>
2017-03-09 18:00:13 +0300

    Выделены вспомогательные классы и методы

---
+++ contact.py
@@ -0,0 +1,13 @@
+class Contact:
+
+    def __init__(self, Firstname, Lastname, Nickname, Title, Company, Address, Homephone, Mobilephone, Email, Homepage):
+        self.Firstname = Firstname
+        self.Lastname = Lastname
+        self.Nickname = Nickname
+        self.Title = Title
+        self.Company = Company
+        self.Address = Address
+        self.Homephone = Homephone
+        self.Mobilephone = Mobilephone
+        self.Email = Email
+        self.Homepage = Homepage
--- test_add_contact.py
+++ test_add_contact.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from selenium.webdriver.firefox.webdriver import WebDriver
-from selenium.webdriver.common.action_chains import ActionChains
 import time, unittest
+from contact import Contact
 
 def is_alert_present(wd):
     try:
@@ -18,60 +18,61 @@ class test_add_contact(unittest.TestCase):
     def test_test_add_contact(self):
         wd = self.wd
         self.open_home_page(wd)
-        self.login(wd)
+        self.login(wd, username="admin", password="secret")
         self.open_new_add(wd)
-        self.create_new_adress(wd)
+        self.create_new_adress(wd, Contact(Firstname="Mikhail", Lastname="Elagin", Nickname="MK", Title="Test", Company="Test", Address="qwerty", Homephone="32145678", Mobilephone="324234234",
+                               Email="1@2.ru", Homepage="www.234.ru"))
         self.return_to_addbook(wd)
         self.logout(wd)
 
     def open_home_page(self, wd):
         wd.get("http://localhost/addressbook/delete.php?part=2;3;")
 
-    def login(self, wd):
-        # Вход в систему. логин и пароль
+    def login(self, wd, username, password):
+        # Вход в систему, ввод логина и пароля
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("user").send_keys(username)
         wd.find_element_by_name("pass").click()
         wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
 
     def open_new_add(self, wd):
         wd.find_element_by_link_text("add new").click()
 
-    def create_new_adress(self, wd):
+    def create_new_adress(self, wd, contact):
         # Заполнение полей формы добавления записи адресной книги
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys("Mikhail")
+        wd.find_element_by_name("firstname").send_keys(contact.Firstname)
         wd.find_element_by_name("lastname").click()
         wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys("Elagin")
+        wd.find_element_by_name("lastname").send_keys(contact.Lastname)
         wd.find_element_by_name("nickname").click()
         wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys("MK")
+        wd.find_element_by_name("nickname").send_keys(contact.Nickname)
         wd.find_element_by_name("title").click()
         wd.find_element_by_name("title").clear()
-        wd.find_element_by_name("title").send_keys("Test")
+        wd.find_element_by_name("title").send_keys(contact.Title)
         wd.find_element_by_name("company").click()
         wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys("Test")
+        wd.find_element_by_name("company").send_keys(contact.Company)
         wd.find_element_by_name("address").click()
         wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys("qwerty")
+        wd.find_element_by_name("address").send_keys(contact.Address)
         wd.find_element_by_name("home").click()
         wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys("32145678")
+        wd.find_element_by_name("home").send_keys(contact.Homephone)
         wd.find_element_by_name("mobile").click()
         wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys("324234234")
+        wd.find_element_by_name("mobile").send_keys(contact.Mobilephone)
         wd.find_element_by_name("email").click()
         wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys("1@2.ru")
+        wd.find_element_by_name("email").send_keys(contact.Email)
         wd.find_element_by_name("homepage").click()
         wd.find_element_by_name("homepage").clear()
-        wd.find_element_by_name("homepage").send_keys("www.234.ru")
+        wd.find_element_by_name("homepage").send_keys(contact.Homepage)
         if not wd.find_element_by_xpath("//div[@id='content']/form/select[1]//option[28]").is_selected():
             wd.find_element_by_xpath("//div[@id='content']/form/select[1]//option[28]").click()
         if not wd.find_element_by_xpath("//div[@id='content']/form/select[2]//option[4]").is_selected():
