5f9621de2c9b66af518981fc24485a183d7f3822
elaginm <elaginm@gmail.com>
2017-03-20 22:10:18 +0300

    Добалвение проверок валидности фикстуры и состоянии сессии

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -2,12 +2,8 @@ from model.group import Group
 
 
 def test_edit_name(app):
-    app.session.login(username="admin", password="secret")
     app.group.edit_first_group(Group(name="Users"))
-    app.session.logout()
 
 
 def test_edit_header(app):
-    app.session.login(username="admin", password="secret")
     app.group.edit_first_group(Group(header="IT"))
-    app.session.logout()
