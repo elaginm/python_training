488461961c662d2a582fcf2b500dd728029f74f3
elaginm <elaginm@gmail.com>
2017-04-05 23:44:20 +0300

    Параметризация тестов для создания групп и контактов

--- fixture/contact.py
+++ fixture/contact.py
@@ -108,12 +108,15 @@ class ContactHelper:
             self.contact_cache = []
             for row in wd.find_elements_by_name("entry"):
                 cells = row.find_elements_by_tag_name("td")
+                id = cells[0].find_element_by_tag_name("input").get_attribute("value")
                 firstname = cells[2].text
                 lastname = cells[1].text
-                id = cells[0].find_element_by_tag_name("input").get_attribute("value")
+                address = cells[3].text
+                all_emails = cells[4].text
                 all_phones = cells[5].text
                 self.contact_cache.append(
-                    Contact(firstname=firstname, lastname=lastname, id=id, all_phones_from_home_page=all_phones))
+                    Contact(id=id, firstname=firstname, lastname=lastname, address=address,
+                            all_emails_from_home_page=all_emails, all_phones_from_home_page=all_phones))
         return list(self.contact_cache)
 
     def open_contact_to_edit_by_index(self, index):
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,27 +1,50 @@
-# -*- coding: utf-8 -*-
 from model.contact import Contact
+import pytest
+import random
+import string
 
 
-def test_add_contact(app):
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + " "*5
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+def random_string_day():
+    return random.randint(1, 31)
+
+
+def random_string_month():
+    month = ['January', 'February', 'March', 'April', 'May', 'June', 'July',
+             'August', 'September', 'October', 'November', 'December']
+    return random.choice(month)
+
+
+def random_string_year():
+    return random.randint(0, 2030)
+
+
+testdata = [Contact(firstname="", lastname="", address="", homephone="", mobilephone="", email="",
+                    birthdayday="", birthdaymonth="-", birthdayyear="")] + [
+            Contact(firstname=random_string("firstname", 10),
+                    lastname=random_string("lastname", 10),
+                    address=random_string("address", 20),
+                    homephone=random_string("", 9),
+                    mobilephone=random_string("", 9),
+                    email=random_string("email@", 15),
+                    birthdayday=str(random_string_day()),
+                    birthdaymonth=random_string_month(),
+                    birthdayyear=str(random_string_year()))
+            for i in range(2)
+            ]
+
+@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
+def test_add_contact(app, contact):
     old_contacts = app.contact.get_contact_list()
-    contact = Contact(firstname="Mikhail", lastname="Elagin", nickname="MK", title="Test", company="Test",
-                      address="qwerty", homephone="32145678", mobilephone="324234234", email="1@2.ru",
-                      homepage="www.234.ru", birthdayday="26", birthdaymonth="March", birthdayyear="1985",
-                      address2="No address")
     app.contact.create(contact)
     assert len(old_contacts) + 1 == app.contact.count()
     new_contacts = app.contact.get_contact_list()
     old_contacts.append(contact)
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
-
-
-def test_add_empty_contact(app):
-    old_contacts = app.contact.get_contact_list()
-    contact = Contact(firstname="", lastname="", nickname="", title="", company="", address="", homephone="",
-                      mobilephone="", email="", homepage="", birthdayday="", birthdaymonth="-", birthdayyear="",
-                      address2="")
-    app.contact.create(contact)
-    assert len(old_contacts) + 1 == app.contact.count()
-    new_contacts = app.contact.get_contact_list()
-    old_contacts.append(contact)
-    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
+#При сравнении списков периодически(т.к. пробелы в тесте рандомны) возникает ошибка, из-за того что в списке
+# new_contacts данные берутся с домашней страницы, где в полях сравнения(firstname, lastname)
+# двойные пробелы не отображаются. Из-за этого при сравнении списков old и new появлется разница
